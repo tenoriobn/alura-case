2b7b8f3c5f69cb60888a92d6a8adeb49e325903e
Bruno Tenório <tenoriobn@gmail.com>
2024-10-01 11:16:44 -0300

    Ajustando next.config.js para ter redirects

--- next.config.js
+++ next.config.js
@@ -1,3 +1,13 @@
-// module.exports = {
-//   output: 'export',
-// };
+module.exports = {
+  // output: 'export',
+  trailingSlash: true,
+  async redirects() {
+    return [
+      {
+        source: '/perguntas',
+        destination: '/faq/',
+        permanent: true,
+      },
+    ]
+  },
+};
