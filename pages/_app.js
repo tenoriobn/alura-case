545114618584c1abee44895868bf47a78602c440
Bruno Tenório <tenoriobn@gmail.com>
2024-10-01 11:28:12 -0300

    estilizando página do FAQ

--- pages/_app.js
+++ pages/_app.js
@@ -5,9 +5,9 @@ function MyApp({ Component, pageProps }) {
     return (
         <>
             <Head>
-                <link rel="preconnect" href="https://fonts.googleapis.com"></link>
-                <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin></link>
-                <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300..800;1,300..800&display=swap" rel="stylesheet"></link>
+                <link rel="preconnect" href="https://fonts.googleapis.com" />
+                <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin={true.toString()} />
+                <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300..800;1,300..800&display=swap" rel="stylesheet" />
             </Head>
             <GlobalStyle />
             <Component {...pageProps} />
--- pages/faq.js
+++ pages/faq.js
@@ -1,6 +1,6 @@
-import Head from 'next/head';
-import Link from '../src/components/Link';
-import PageTitle from '../src/components/PageTitle';
+import FAQScreen from '../src/screens/FAQScreen';
+
+export default FAQScreen;
 
 export async function getStaticProps() {
     const FAQ_API_URL = 'https://gist.githubusercontent.com/omariosouto/0ceab54bdd8182cbd1a4549d32945c1a/raw/578ad1e8e5296fa048e3e7ff6b317f7497b31ad9/alura-cases-faq.json';
@@ -19,33 +19,4 @@ export async function getStaticProps() {
             faq
         }
     }
-}
-
-export default function FAQPage({ faq }) {
-    console.log(faq)
-
-    console.log('isso tá rodando aqui no servidor DEV?')
-    return (
-        <div>
-            <PageTitle>
-                FAQ - Alura Cases Campanha
-            </PageTitle>
-
-            <h1>Alura Cases - Páginas de Perguntas FAQ</h1>
-            <Link href="/">
-                Ir para a home
-            </Link>
-
-            <ul>
-                {faq.map(({ answer, question}) => (
-                    <li key={question}>
-                        <article>
-                            <h2>{question}</h2>
-                            <p>{answer}</p>
-                        </article>
-                    </li>
-                ))}
-            </ul>
-        </div>
-    )
 }
